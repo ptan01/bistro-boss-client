29ee3051160e82b6dfab68ace1476acc6cd5e76c
ptan01 <ptanjim01@gmail.com>
2023-05-25 11:06:12 +0600

    create login page and use captcha

--- src/leyout/Main.jsx
+++ src/leyout/Main.jsx
@@ -1,13 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavBar from '../shared/NavBar';
 import Footer from '../shared/Footer';
 
 const Main = () => {
+    const location = useLocation()
+
+    console.log(location)
+    const islogin = location.pathname.includes('login')
+
+
     return (
         <div>
-            <NavBar></NavBar>
+            { islogin || <NavBar></NavBar>}
             <Outlet></Outlet>
-            <Footer></Footer>
+            { islogin || <Footer></Footer>}
         </div>
     );
 };
