0153cb4fd604e02e83baa9210028b4690c6ecc6e
ptan01 <ptanjim01@gmail.com>
2023-05-29 19:46:08 +0600

    make user admin function done

--- src/pages/dashboardPages/allUser/AllUser.jsx
+++ src/pages/dashboardPages/allUser/AllUser.jsx
@@ -1,16 +1,69 @@
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { FaTrashAlt } from "react-icons/fa";
+import { HiUserGroup } from 'react-icons/hi';
 
 
 const AllUser = () => {
 
-    const {data : users = [] , refetch} = useQuery(['users'], async()=> {
+    const { data: users = [], refetch } = useQuery(['users'], async () => {
         const res = await fetch('http://localhost:5000/users')
         return res.json()
-    } )
+    })
+
+
+    const handleMakeAdmin = (id)=> {
+        console.log(id)
+        fetch(`http://localhost:5000/users/admin/${id}`, {
+            method: 'PATCH',
+           
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
+
+
+    const handleDelete = () => {
+
+    }
 
     return (
-        <div>
-            <h1>All user {users.length} </h1>
+        <div className="w-full">
+            <Helmet>
+                <title>Bistro Boss | All User</title>
+            </Helmet>
+            <h1 className="text-4xl font-bold">Total User : {users.length} </h1>
+
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, idx) => <tr key={user._id}>
+                                <th>{idx}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td> <button onClick={() => handleMakeAdmin(user._id)} className="btn btn-ghost bg-[#D1A054] btn-xl"><HiUserGroup className="text-2xl"></HiUserGroup></button></td>
+                                <td> <button className="btn btn-ghost bg-red-700 btn-xl"><FaTrashAlt className="text-2xl"></FaTrashAlt></button></td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 };
