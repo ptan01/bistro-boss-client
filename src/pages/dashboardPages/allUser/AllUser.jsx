9913c7682833d80dcbfcb7134d2e324fe038a111
ptan01 <ptanjim01@gmail.com>
2023-05-30 09:10:20 +0600

    when user admin done then swal alart added

--- src/pages/dashboardPages/allUser/AllUser.jsx
+++ src/pages/dashboardPages/allUser/AllUser.jsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { FaTrashAlt } from "react-icons/fa";
 import { HiUserGroup } from 'react-icons/hi';
+import Swal from "sweetalert2";
 
 
 const AllUser = () => {
@@ -12,20 +13,30 @@ const AllUser = () => {
     })
 
 
-    const handleMakeAdmin = (id)=> {
+    const handleMakeAdmin = (id) => {
         console.log(id)
         fetch(`http://localhost:5000/users/admin/${id}`, {
             method: 'PATCH',
-           
+
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                if(data.modifiedCount > 0){
+                    refetch()
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: 'Admin has been added',
+                        showConfirmButton: false,
+                        timer: 1000
+                      })
+                }
+                console.log(data)
+            })
     }
 
 
-
+    // TODO: USER DELETE NOT COMPLITE
     const handleDelete = () => {
 
     }
@@ -55,7 +66,7 @@ const AllUser = () => {
                                 <th>{idx}</th>
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
-                                <td> <button onClick={() => handleMakeAdmin(user._id)} className="btn btn-ghost bg-[#D1A054] btn-xl"><HiUserGroup className="text-2xl"></HiUserGroup></button></td>
+                                <td>{user.role ? <p>Admin</p> : <button onClick={() => handleMakeAdmin(user._id)} className="btn btn-ghost bg-[#D1A054] btn-xl"><HiUserGroup className="text-2xl"></HiUserGroup></button>}</td>
                                 <td> <button className="btn btn-ghost bg-red-700 btn-xl"><FaTrashAlt className="text-2xl"></FaTrashAlt></button></td>
                             </tr>)
                         }
