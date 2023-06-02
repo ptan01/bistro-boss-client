33721e8bbfb52ab9f287f435c03d285ceab287be
ptan01 <ptanjim01@gmail.com>
2023-06-02 12:50:28 +0600

    success fully payment implement complete in payment comop

--- src/pages/dashboardPages/payment/CheckoutFrom.jsx
+++ src/pages/dashboardPages/payment/CheckoutFrom.jsx
@@ -1,47 +1,113 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useEffect, useState } from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import useAuth from '../../../hooks/useAuth';
 
-const CheckoutFrom = () => {
+const CheckoutFrom = ({price}) => {
 
-    const stripe = useStripe()
-    const elements = useElements();
-
-    const handleSubmit = async (event)=>{
-        event.preventDefault()
-        if(!stripe || !elements){
-            return
-        }
-
-        const card = elements.getElement(CardElement) ;
-        if(card === null){
-            return ;
-        }
+  console.log(price)
+  const [axiosInstance] = useAxiosSecure()
+  const stripe = useStripe()
+  const {user} = useAuth()
+  const elements = useElements();
+  const [cardError, setCardError] = useState('')
+  const [clientSecret, setClientSecret] = useState("")
+  const [processing , setProcessing] = useState(false)
+  const [transactionID, setTransactionID] = useState('')
 
 
+  useEffect(()=>{
+    console.log(price)
+    axiosInstance.post('/create-payment-intent', {price})
+    .then(res =>{
+      console.log(res.data.clientSecret)
+      setClientSecret(res.data.clientSecret)
+    })
+  },[])
+
+
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    if (!stripe || !elements) {
+      return
     }
 
-    return (
-         <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+    const card = elements.getElement(CardElement);
+    if (card === null) {
+      return;
+    }
+
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card
+    })
+
+    if (error) {
+      setCardError(error.message)
+      console.log(error)
+    }
+    else {
+      setCardError('')
+      console.log(paymentMethod)
+    }
+    setProcessing(true)
+    const {paymentIntent, error: confirmError} = await stripe.confirmCardPayment(
+      clientSecret,
+      {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || 'unknown',
+            name: user?.displayName || 'anonymous'
+          },
+        },
+      },
+    );
+
+    if(confirmError){
+      setCardError(confirmError.message)
+    }
+    setProcessing(false)
+    if(paymentIntent.status === 'succeeded'){
+      setTransactionID(paymentIntent.id)
+    }
+    console.log(paymentIntent)
+
+
+  }
+
+  console.log(clientSecret)
+
+  return (
+    <div className='w-2/3 mx-auto'>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: '16px',
+                color: '#424770',
+                '::placeholder': {
+                  color: '#aab7c4',
+                },
+              },
+              invalid: {
+                color: '#9e2146',
               },
             },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
-    );
+          }}
+        />
+        <button className="btn btn-outline btn-primary btn-sm mt-5" type="submit" disabled={!stripe || !clientSecret || processing}>
+          Pay
+        </button>
+      </form>
+      { cardError && <p className='text-red-600'>{cardError}</p>}
+      {transactionID && <p className='text-green-600'>Transaction complete with Transaction ID : {transactionID}</p>}
+    </div>
+  );
 };
 
 export default CheckoutFrom;
--- src/pages/dashboardPages/payment/Payment.jsx
+++ src/pages/dashboardPages/payment/Payment.jsx
@@ -3,22 +3,28 @@ import SectionTitle from "../../../components/sectionTitle/SectionTitle";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutFrom from "./CheckoutFrom";
 import { loadStripe } from "@stripe/stripe-js";
+import useCart from "../../../hooks/useCart";
 
 
 
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK)
 
 const Payment = () => {
+
+    const [card] = useCart()
+    const totalprice = card.reduce((previous, current)=> previous + current.price , 0)
+    const total = parseFloat(totalprice.toFixed(2))
+    console.log(total)
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss || Payment</title>
             </Helmet>
             <SectionTitle subHeading='please process' heading='Payment'></SectionTitle>
-            <h1>taka o poysa tumi uira uira jao</h1>
+            <h1>taka o poysa tumi uira uira jao (Ai page reload dile fetch korte pare na so reload dio na)</h1>
 
             <Elements stripe={stripePromise}>
-                <CheckoutFrom></CheckoutFrom>
+                <CheckoutFrom price={total}></CheckoutFrom>
             </Elements>
         </div>
     );
